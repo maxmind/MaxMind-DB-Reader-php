034c25727c2782f8408d7f39f46b1c0a1ce5afd3
Gregory Oschwald <goschwald@maxmind.com>
2015-01-12 15:28:58 -0800

    Display diff before releasing

--- dev-bin/make-release.sh
+++ dev-bin/make-release.sh
@@ -12,18 +12,17 @@ use v5.16;
 my \$log = read_file(q{CHANGELOG.md});
 \$log =~ /\n(\d+\.\d+\.\d+) \((\d{4}-\d{2}-\d{2})\)\n/;
 die 'Release time is not today!' unless DateTime->now->ymd eq \$2;
-die 'No version detectd' unless \$1;
 say \$1;
 EOF
 )
 
-perl -pi -e 's/(?<=#define PHP_MAXMINDDB_VERSION ")\d+\.\d+\.\d+(?=")/$VERSION/' ext/php_maxminddb.h
+perl -pi -e "s/(?<=#define PHP_MAXMINDDB_VERSION \")\d+\.\d+\.\d+(?=\")/$VERSION/" ext/php_maxminddb.h
 
 git diff
 
 git commit -m "Bumped version to $VERSION" -a
 
-git tag -a -m "Release for $VERSION" $VERSION
+git tag -a -m "Release for $VERSION" "v$VERSION"
 
 
 read -p "Push to origin? (y/n) " SHOULD_PUSH
