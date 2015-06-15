7fa956d9571f553ddf7207dde8c694f208f3a953
Gregory Oschwald <goschwald@maxmind.com>
2015-06-15 10:37:41 -0700

    Don't run phpcs on nightly

--- .travis-test.sh
+++ .travis-test.sh
@@ -5,12 +5,17 @@ set -x
 
 mkdir -p build/logs
 phpunit -c .coveralls-phpunit.xml.dist
-if [ "hhvm" != "$(phpenv version-name)" ]
+
+if [ "hhvm" != $TRAVIS_PHP_VERSION ]
 then
-    phpcs --standard=PSR2 src/
     echo "mbstring.internal_encoding=utf-8" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
     echo "mbstring.func_overload = 7" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
     phpunit
     echo "extension = ext/modules/maxminddb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
     phpunit
+fi
+
+if [ "hhvm" != $TRAVIS_PHP_VERSION ] && [ "nightly" != $TRAVIS_PHP_VERSION ]
+then
+    phpcs --standard=PSR2 src/
 fi
