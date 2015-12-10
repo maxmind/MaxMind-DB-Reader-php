1a36550538c87081a640cd8d25564572ee5a2ee6
Gregory Oschwald <goschwald@maxmind.com>
2015-12-10 07:18:23 -0800

    Use phpunit from Composer with Travis

--- .travis-test.sh
+++ .travis-test.sh
@@ -6,16 +6,17 @@ set -x
 mkdir -p build/logs
 phpunit -c .coveralls-phpunit.xml.dist
 
-if [ "hhvm" != $TRAVIS_PHP_VERSION ]
+if [ "hhvm" != "$TRAVIS_PHP_VERSION" ]
 then
     echo "mbstring.internal_encoding=utf-8" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
     echo "mbstring.func_overload = 7" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-    phpunit
+    ./vendor/bin/phpunit
+
     echo "extension = ext/modules/maxminddb.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-    phpunit
+    ./vendor/bin/phpunit
 fi
 
-if [ "hhvm" != $TRAVIS_PHP_VERSION ] && [ "7.0" != $TRAVIS_PHP_VERSION ]
+if [ "hhvm" != "$TRAVIS_PHP_VERSION" ] && [ "7.0" != "$TRAVIS_PHP_VERSION" ]
 then
     phpcs --standard=PSR2 src/
 fi
