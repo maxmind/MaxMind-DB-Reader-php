e3d2c8b76533893e1b20c480dab5e66ec37f4bbe
Edward HeXuefei <xuphey@gmail.com>
2018-09-25 15:22:32 +0800

    check for existency before loading any classes

--- autoload.php
+++ autoload.php
@@ -36,7 +36,9 @@ function mmdb_autoload($class)
         $path = $path . '.php';
 
         /* $path should now contain the path to a PHP file defining $class */
-        @include $path;
+        if (file_exists($path)) {
+            include $path;
+        }
     }
 }
 
