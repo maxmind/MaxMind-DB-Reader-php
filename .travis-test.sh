4d0529db303a9a4bc3127d2240bc4d9ffe4284cf
Andy Jack <ajack@maxmind.com>
2019-02-19 18:21:59 -0500

    Remove hhvm support from readme/travis config & scripts

--- .travis-test.sh
+++ .travis-test.sh
@@ -6,15 +6,12 @@ set -x
 mkdir -p build/logs
 ./vendor/bin/phpunit -c .coveralls-phpunit.xml.dist
 
-if [ "hhvm" != "$TRAVIS_PHP_VERSION" ]
-then
-    echo "mbstring.internal_encoding=utf-8" >> ~/.phpenv/versions/"$(phpenv version-name)"/etc/php.ini
-    echo "mbstring.func_overload = 7" >> ~/.phpenv/versions/"$(phpenv version-name)"/etc/php.ini
-    ./vendor/bin/phpunit
+echo "mbstring.internal_encoding=utf-8" >> ~/.phpenv/versions/"$(phpenv version-name)"/etc/php.ini
+echo "mbstring.func_overload = 7" >> ~/.phpenv/versions/"$(phpenv version-name)"/etc/php.ini
+./vendor/bin/phpunit
 
-    echo "extension = ext/modules/maxminddb.so" >> ~/.phpenv/versions/"$(phpenv version-name)"/etc/php.ini
-    ./vendor/bin/phpunit
-fi
+echo "extension = ext/modules/maxminddb.so" >> ~/.phpenv/versions/"$(phpenv version-name)"/etc/php.ini
+./vendor/bin/phpunit
 
 if [ $RUN_LINTER ]; then
     vendor/bin/php-cs-fixer fix --verbose --diff --dry-run --config=.php_cs
