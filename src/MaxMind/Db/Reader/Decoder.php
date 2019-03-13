5d40621abbd3ee72772a757a46586e4e7dd324d5
Will Storey <will@summercat.com>
2019-03-12 17:49:07 -0700

    Hide several defines from phpdoc

--- src/MaxMind/Db/Reader/Decoder.php
+++ src/MaxMind/Db/Reader/Decoder.php
@@ -19,21 +19,37 @@ class Decoder
     private $pointerTestHack;
     private $switchByteOrder;
 
+    /** @ignore */
     const _EXTENDED = 0;
+    /** @ignore */
     const _POINTER = 1;
+    /** @ignore */
     const _UTF8_STRING = 2;
+    /** @ignore */
     const _DOUBLE = 3;
+    /** @ignore */
     const _BYTES = 4;
+    /** @ignore */
     const _UINT16 = 5;
+    /** @ignore */
     const _UINT32 = 6;
+    /** @ignore */
     const _MAP = 7;
+    /** @ignore */
     const _INT32 = 8;
+    /** @ignore */
     const _UINT64 = 9;
+    /** @ignore */
     const _UINT128 = 10;
+    /** @ignore */
     const _ARRAY = 11;
+    /** @ignore */
     const _CONTAINER = 12;
+    /** @ignore */
     const _END_MARKER = 13;
+    /** @ignore */
     const _BOOLEAN = 14;
+    /** @ignore */
     const _FLOAT = 15;
 
     public function __construct(
