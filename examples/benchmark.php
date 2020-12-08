2b34280816c65093f1e5d9a6de35a9fe1b53b3bc
Gregory Oschwald <goschwald@maxmind.com>
2020-12-08 07:43:46 -0800

    Run new php-cs-fixer against code

--- examples/benchmark.php
+++ examples/benchmark.php
@@ -10,7 +10,7 @@ $reader = new Reader('GeoIP2-City.mmdb');
 $count = 50000;
 $startTime = microtime(true);
 for ($i = 0; $i < $count; ++$i) {
-    $ip = long2ip(rand(0, pow(2, 32) - 1));
+    $ip = long2ip(rand(0, 2 ** 32 - 1));
     $t = $reader->get($ip);
     if ($i % 1000 === 0) {
         echo $i . ' ' . $ip . "\n";
