b4504822a2463a3c11baffdc64304bf88af05e7d
Laura Taylor <lauratayloruk@ionos.gr>
2023-09-08 14:26:04 +0300

    The Linter is misbehaving... reported errors conflicr with each other

--- src/MaxMind/Db/Reader/InvalidDatabaseException.php
+++ src/MaxMind/Db/Reader/InvalidDatabaseException.php
@@ -7,4 +7,6 @@ namespace MaxMind\Db\Reader;
 /**
  * This class should be thrown when unexpected data is found in the database.
  */
-class InvalidDatabaseException extends \Exception {}
+class InvalidDatabaseException extends \Exception
+{
+}
